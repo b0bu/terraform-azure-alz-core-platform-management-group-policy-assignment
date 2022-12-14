f56165673a63685e64e2429b2f79b77d05dd45e3
Jason Leitch <jasonleitch0@gmail.com>
2022-12-14 09:40:31 +0000

    chore: add tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+// these policies can remediate and therefore require a managed identity
+resource "azurerm_management_group_policy_assignment" "assignments" {
+  name                 = var.name
+  policy_definition_id = var.policy_id
+  management_group_id  = var.management_group_id
+  parameters           = var.parameters
+
+  location = "uksouth"
+  identity {
+    type = "SystemAssigned"
+  }
+}
