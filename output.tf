df67fa1e100b9d843bdcb618faf8c0a216d1291e
Jason Leitch <jasonleitch0@gmail.com>
2022-12-15 11:40:33 +0000

    chore: update name

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 // output identity id after creation?
 output "identity" {
-  value = azurerm_management_group_policy_assignment.assignments.identity
+  value = azurerm_management_group_policy_assignment.assignment.identity
 }
